14c61a094c7dec05cb69f549a275534cf9c37a80
Ndimphiwe <39130059+ndimphiwem@users.noreply.github.com>
2020-03-22 18:29:10 +0200

    refactor (bio): Removed frontend pivot  comments

--- src/app/content/bio.ts
+++ src/app/content/bio.ts
@@ -1,12 +1,9 @@
 export const bio = {
     heading: 'Web and UI/UX Developer based in Centurion.',
-    content: `I'm an experienced developer with varied experience in both back-end and front-end
+    content: `I'm an experienced Web and UI/UX developer with varied experience in both back-end and front-end
     development technologies ranging from SQL, C# Java, and the MVC framework to
     JavaScript, HTML5 and CSS along with front-end frameworks like Angular, Bootstrap,
     Material Design Bootstrap, and JQuery.<br><br>
     The challenge that drives me is the blending of back-end logic with the creativity of front-end
-    presentation to solve problems or bring ideas to life.<br><br>
-    I've also made a more front-end focused pivot in my career, and to that end, Angular has
-    become my flavour of development with JavaScript, CSS, and HTML all having a core role in
-    my toolbox, than in previous years where I was primarily a full stack web developer.`
+    presentation to solve problems or bring ideas to life.`
 };
